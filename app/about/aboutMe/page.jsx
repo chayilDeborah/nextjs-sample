bdc05f37784efd4587984c833657f095130fe4c2
chayilDeborah <aniyikayeolamide3@gmail.com>
2023-05-19 16:40:28 +0100

    feat: create AboutMe folder 2 learn nested routing

---
+++ app/about/aboutMe/page.jsx
@@ -0,0 +1,10 @@
+const AboutMe = () => {
+    return (
+        <>
+        <h1 className="flex justify-center mt-[50px]">This is the About Me page</h1>
+        <div className="flex justify-center text-1xl items-center mt-[30px]">I'm Olamide Deborah, a frontend developer with more than one year of experience writing Frontend technologies (React,Javascript Framework ), HTML and CSS </div>
+        <div className="text-1xl flex justify-center mt-[30px]" >I love asking questions and connecting with like minds</div>
+        </>
+    )
+}
+export default AboutMe
