9badb53561a9d42902cd80e13f42b82737913c4a
chayilDeborah <aniyikayeolamide3@gmail.com>
2023-05-27 21:03:10 +0100

    feat: caching and revalidating

--- app/code/repos/page.jsx
+++ app/code/repos/page.jsx
@@ -2,7 +2,13 @@ import Link from 'next/link';
 import { FaStar, FaCodeBranch, FaEye} from 'react-icons/fa'
 
 async function fetchRepos() {
-    const response = await fetch('https://api.github.com/users/chayilDeborah/repos');
+    const response = await fetch('https://api.github.com/users/chayilDeborah/repos',
+    {
+        next: {
+          revalidate: 60,
+        }
+      });
+    
 
     await new Promise((resolve) => setTimeout(resolve, 3000))
     const repos = await response.json();
