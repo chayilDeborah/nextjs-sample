c5a289be8ff4388f8797712d972cef36171c1bb0
chayilDeborah <aniyikayeolamide3@gmail.com>
2023-05-27 06:37:13 +0100

    feat: import repo page to  display selected repo

---
+++ app/code/repos/[name]/page.jsx
@@ -0,0 +1,13 @@
+import Repo from "@/app/components/Repo"
+import Link from "next/link"
+
+const RepoPage = ({ params: {name} }) => {
+  return (
+    <div className='card'>
+        <Link href='/code/repos' className="btn btn-back">Back to Repositories</Link>
+        <Repo name={name}/>
+    </div>
+  )
+}
+
+export default RepoPage
