bb433d2ccd84b14f32a9717ff72e0e50bea64f7a
chayilDeborah <aniyikayeolamide3@gmail.com>
2023-05-29 00:00:14 +0100

    feat: create state functions to handle loading

--- app/page.js
+++ app/page.js
@@ -1,11 +1,34 @@
-import Link from 'next/link'
-import Courses from './components/Courses'
+"use client";
+import { useState, useEffect } from "react";
+
+import Link from "next/link";
+
+import Courses from "./components/Courses";
+import LoadingPage from "./loading";
 
 export default function Home() {
+  const [courses, setCourses] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchCourses = async () => {
+      const res = await fetch("/api/courses");
+      const data = await res.json();
+      setCourses(data);
+      setLoading(false);
+    };
+
+    fetchCourses();
+  }, []);
+
+  if (loading) {
+    return <LoadingPage />;
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <button className='btn text-2xl bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-md p-5 hover:from-pink-500 hover:to-indigo-500'>
+        <button className="btn text-2xl bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-md p-5 hover:from-pink-500 hover:to-indigo-500">
           Testing NextJs
         </button>
       </div>
@@ -13,12 +36,15 @@ export default function Home() {
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
         This is cooool
       </div>
-<Courses />
+      <Courses courses={courses} />
       <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <div><Link href='/about'>About Page</Link></div>
-        <div><Link href='/about/aboutMe'>Further intro about Me</Link></div>
+        <div>
+          <Link href="/about">About Page</Link>
+        </div>
+        <div>
+          <Link href="/about/aboutMe">Further intro about Me</Link>
+        </div>
       </div>
-
     </main>
-  )
+  );
 }
